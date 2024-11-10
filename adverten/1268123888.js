9b48d0ae3aa4fb14fbe81d0f7eb4000e130a053f
tubemoca <adstubemoca@gmail.com>
2024-11-10 11:53:45 +0700

    Update 1268123888.js

--- adverten/1268123888.js
+++ adverten/1268123888.js
@@ -2,11 +2,11 @@ function generateClickId() {
     let clickIdBase64 = '';
 
     do {
-        const randomString = Math.random().toString(36).substring(2, 12); // String acak
-        const timestamp = Date.now().toString(); // Timestamp saat ini
+        const randomString = Math.random().toString(36).substring(2, 12);
+        const timestamp = Date.now().toString();
 
         const clickIdRaw = `${randomString}|${randomString}|${randomString}|${timestamp}|0`;
-        clickIdBase64 = btoa(clickIdRaw); // Base64 encoding
+        clickIdBase64 = btoa(clickIdRaw); 
     } while (!clickIdBase64.startsWith("Fkpst"));
 
     return clickIdBase64;
@@ -24,11 +24,11 @@ function createTrackingUrl(campaign_id, target_url) {
     return trackingUrl;
 }
 
-// Contoh penggunaan fungsi dengan campaign_id dan target_url
-const campaign_id = "12345";  // Ganti dengan ID kampanye Anda
-const target_url = "https://s.tubesplash.net/spots/482716?&s1=nav_bar&kw=sex%20for%20you&s2=hellporner.com"; // Target URL yang diinginkan
+
+const campaign_id = "1268123888";
+const target_url = "https://s.tubesplash.net/spots/482716?&s1=nav_bar&kw=sex%20for%20you&s2=hellporner.com";
 const trackingUrl = createTrackingUrl(campaign_id, target_url);
 console.log("Generated tracking URL:", trackingUrl);
 
-// Jika ingin mengarahkan ke URL yang dihasilkan
+
 window.location.href = trackingUrl;
